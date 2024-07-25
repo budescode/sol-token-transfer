f534229215d62aedc9c1ac6811f31b59ab3e77a0
budescode <gospeltruth18@gmail.com>
2024-07-25 13:23:08 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,116 @@
+'use client';
 import Image from "next/image";
+import { FormEvent, useMemo, } from "react";
+import { NextPage } from "next";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import * as web3 from "@solana/web3.js";
+import { WalletConnectButton, WalletDisconnectButton, WalletModal, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+//getting balance
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { FC, useEffect, useState } from "react";
+import { error } from "console";
+
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   
+    const [balance, setBalance] = useState(0);
+    const { connection } = useConnection();
+    const { publicKey,sendTransaction } = useWallet();
+    const [addressToSend, setaddressToSend] = useState("")
+    const [amountToSend, setamountToSend] = useState(0)
+    const sendSol = (
+        event:any
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    ) => {
+        event.preventDefault();
+      
+        const transaction = new web3.Transaction();
+        const recipientPubKey = new web3.PublicKey(addressToSend);
+      
+        const sendSolInstruction = web3.SystemProgram.transfer({
+          fromPubkey: publicKey!,
+          toPubkey: recipientPubKey,
+          lamports: LAMPORTS_PER_SOL * Number(amountToSend),
+        });
+      
+        transaction.add(sendSolInstruction);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        sendTransaction(transaction, connection).then((sig) => {
+            console.log(`Transaction successful: ${sig}`);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        })
+        .catch((error) => {
+            console.error(`Transaction failed: ${error}`);
+          });
+        ;
+      };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    useEffect(() => {
+        if (!connection || !publicKey) {
+            console.log("no connection")
+          return;
+        }
+        console.log("there is connection", connection.rpcEndpoint, publicKey.toString())
+        connection.onAccountChange(
+          publicKey,
+          (updatedAccountInfo) => {
+            setBalance(updatedAccountInfo.lamports / LAMPORTS_PER_SOL);
+          },
+          "confirmed",
+        );
+    
+        connection.getAccountInfo(publicKey).then((info) => {
+            console.log(info, "yeah info")
+            if(info)setBalance(info!.lamports);
+        });
+      }, [connection, publicKey]);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+
+              <WalletMultiButton />
+         
+              <p>{publicKey ? `Balance: ${balance / LAMPORTS_PER_SOL} SOL` : ""}</p>
+              <form onSubmit={sendSol}>
+              <div>
+                    <label>Transfer Address</label>
+                    <input style={{
+                        width: "100%",
+                        padding: "10px",
+                        border: '1px solid #ccc',   
+                    }}
+                    value = {addressToSend}
+                    onChange={(e)=>setaddressToSend(e.target.value)}
+                    required
+                    />
+
+                </div>
+
+                <div>
+                    <label>Transfer Amount</label>
+                    <input style={{
+                        width: "100%",
+                        padding: "10px",
+                        border: '1px solid #ccc',
+                    }}
+                    type="number"
+                    onChange={(e)=> {
+                    e?    setamountToSend(Number(e.target.value)) : null
+                    }}
+                    value = {amountToSend}
+                    required
+                    />
+                </div>
+
+                <button type="submit">Submit</button>
+
+              </form>
+              {/* <button onClick={sendSol}>Send sol t</button> */}
+          </main>  
+      );
 }
